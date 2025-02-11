4b77b6b971609c8fd030f92b432d65310e4ee1c5
Aleksey <aleksshugol2911@gmail.com>
2025-02-11 15:09:15 +0300

    gui

---
+++ src/main/python/GUI/serverGUI.py
@@ -0,0 +1,17 @@
+import socket
+import json
+import os
+import threading
+import datetime
+from json import JSONDecodeError
+from urllib.parse import unquote
+from tkinter import *
+
+from ..modules.database.serverDB import ServerDB
+from ..modules.token.jwtManager import JWTManager
+
+class ServerGui:
+    def __init__(self):
+        pass
+
+
--- src/main/python/start.py
+++ src/main/python/start.py
@@ -1,5 +1,8 @@
 from src.main.python.CLI.server import Server
+from modules.functions.os.check_architecrure import check_system_bitness as check
 
 if __name__ == "__main__":
+    bitness = check()
+    print(bitness)
     server = Server("../../../config/config.json")
     server.start()
