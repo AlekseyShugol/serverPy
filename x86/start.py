72059f196f2dc7fbb968fa43d861a03c562ba095
Aleksey <aleksshugol2911@gmail.com>
2025-02-19 16:41:34 +0300

    gui

--- x86/start.py
+++ x86/start.py
@@ -6,16 +6,15 @@ import tkinter as tk
 # passw = str(input("Enter mode(enter tu gui, cli to console>> "))
 
 if __name__ == "__main__":
-    server = Server("config/config.json")
-    server.start()
-    # bitness = check()
-    # print(bitness)
-    # if bitness == "64bit" and passw != "cli":
-    #     app = MyApp("config/config.json")
-    #     app.start()
-    # elif passw == "cli":
-    #     server = Server("config/config.json")
-    #     server.start()
-    # else:
-    #     server = Server("config/config.json")
-    #     server.start()
+    passw = input("enter mode: ")
+    bitness = check()
+    print(bitness)
+    if bitness == "64bit" and passw != "cli":
+        app = MyApp("config/config.json")
+        app.start()
+    elif passw == "cli":
+        server = Server("config/config.json")
+        server.start()
+    else:
+        server = Server("config/config.json")
+        server.start()
