42bce4d1844200bc42c21514145fe29f8aa1ac27
Aleksey <aleksshugol2911@gmail.com>
2025-02-19 16:53:40 +0300

    new

--- x86/start.py
+++ x86/start.py
@@ -3,10 +3,9 @@ from check_architecrure import check_system_bitness as check
 from serverGUI import MyApp
 import tkinter as tk
 
-# passw = str(input("Enter mode(enter tu gui, cli to console>> "))
+passw = str(input("Enter mode(enter to gui, cli to console>> "))
 
 if __name__ == "__main__":
-    passw = input("enter mode: ")
     bitness = check()
     print(bitness)
     if bitness == "64bit" and passw != "cli":
